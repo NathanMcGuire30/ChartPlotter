8bccabf5569d730abb2538d8e0386fba48ae425e
nathan <nathan.e.mcguire@gmail.com>
2021-09-06 12:25:53 -0400

    large renderer does a bit better

--- Test/Large_Test.py
+++ Test/Large_Test.py
@@ -4,7 +4,6 @@
 Test code for ChartPlotter
 """
 
-import time
 import cv2
 import numpy
 import numpy as np
@@ -17,32 +16,35 @@ if __name__ == '__main__':
     globalLowerLeft = [41.519489, -70.7288717]
     globalLowerRight = [41.550559, -70.6228197]
 
-    boxes = 6
+    boxes = 8
+    boxWidth = 100
 
     latitudes = numpy.linspace(globalLowerLeft[0], globalLowerRight[0], boxes + 1)
     longitudes = numpy.linspace(globalLowerLeft[1], globalLowerRight[1], boxes + 1)
 
     image = None
+    padding = boxes
 
     for i in range(len(latitudes) - 1):
-        row = None
-
         for j in range(len(longitudes) - 1):
             print("{0} of {1}".format(i * boxes + j + 1, boxes * boxes))
 
             lowerLeft = (latitudes[i], longitudes[j])
             upperRight = (latitudes[i + 1], longitudes[j + 1])
-            im = chartPlotter.plotChart(lowerLeft, upperRight, 200)
+            im = chartPlotter.plotChart(lowerLeft, upperRight, boxWidth)
 
-            if row is None:
-                row = im
-            else:
-                row = numpy.concatenate((row, im), axis=1)
+            [imHeight, imWidth, _] = im.shape
 
-        if image is None:
-            image = row
-        else:
-            image = numpy.concatenate((row, image), axis=0)
+            if image is None:
+                image = np.zeros([imHeight * boxes + padding, imWidth * boxes + 1, 3], dtype=np.uint8)
+
+            startRow = (imHeight * boxes) - (imHeight * i) - 1 + padding
+            startColumn = imWidth * j
+
+            image[startRow - imHeight:startRow, startColumn:startColumn + imWidth, 0:2] = im[:, :, 0:2]
+
+            cv2.imshow("Output", image)
+            cv2.waitKey(1)
 
     cv2.imwrite("Output.png", image)
     cv2.imshow("Output", image)
